00696e110f63f46d0b312145ab5209b5ab8b3e65
chrisgarcia7 <garciachristian9@yahoo.com>
2026-02-13 21:20:56 -0600

    Corrigiendo Sidebar

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -7,19 +7,35 @@ export default function Layout() {
   const [abrir, setAbrir] = useState<boolean>(false);
   return (
     <>
-      <Button
-        icon="pi pi-bars"
-        onClick={() => {
-          setAbrir(true);
-        }}
-        rounded
-        text
-        raised
-        severity="secondary"
-        className="m-3"
-      />
-      <Nav abrir={abrir} setAbrir={setAbrir} />
-      <Outlet />
+      <div className="flex">
+        {/* Sidebar */}
+        <Nav abrir={abrir} setAbrir={setAbrir} />
+
+        {/* Contenedor principal */}
+        <main className="flex-1 relative">
+          {/* Botón encima del contenido */}
+          <Button
+            icon="pi pi-bars"
+            onClick={() => setAbrir(true)}
+            rounded
+            text
+            raised
+            severity="secondary"
+            style={{
+              position: "absolute",
+              top: "20px", // Ajusta la distancia desde arriba
+              right: "20px", // Ajusta la distancia desde la derecha
+              zIndex: 1000, // Asegura que esté por encima del Outlet
+              color: "white",
+              border: "none",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }} // z muy alto
+          />
+
+          <Outlet />
+        </main>
+      </div>
     </>
   );
 }
